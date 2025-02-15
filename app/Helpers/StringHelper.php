22e7ef52a8a9b851f2883b7a9338b50f0c6c93fd
Omri Edri <edri90@gmail.com>
2025-02-15 18:25:56 +0200

    Add string cleaning function and update email headers in EmailService

--- app/Helpers/StringHelper.php
+++ app/Helpers/StringHelper.php
@@ -4,6 +4,16 @@ class StringHelper {
 
     public const JSON_ENCODE_BEAUTIFY = JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT;
 
+
+    /**
+     * Function to clean string from any tags and special characters
+     * @param string $string
+     * @return string
+     */
+    public static function cleanSpecialChars(string $string): string {
+        return preg_replace('/[^A-Za-z0-9\-א-ת]/', '', $string);
+    }
+
     /**
      * Function validate if given value is valid string
      * @param mixed $value
--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -1,4 +1,4 @@
-<?php
+<?php require_once __DIR__ . '/../Helpers/StringHelper.php';
 class EmailService {
 
     public static function sendEmail(string $to, string $subject, string $message): bool {
@@ -21,11 +21,11 @@ class EmailService {
      */
     public static function sendNoReplayEmail(string $to, string $subject, string $message): bool {
         $to = filter_var($to, FILTER_SANITIZE_EMAIL);
-        $subject = htmlspecialchars($subject, ENT_QUOTES, 'UTF-8');
-        $headers = "From: " . $_ENV['NO_REPLY__SENDER'] . "\r\n";
-        $headers .= "Reply-To: " . $_ENV['NO_REPLY__EMAIL'] . "\r\n";
-        $headers .= "Return-Path: " . $_ENV['NO_REPLY__EMAIL'] . "\r\n";
-        $headers .= "X-Mailer: PHP/" . phpversion();
+        $subject = StringHelper::cleanSpecialChars($subject);
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From: " . $_ENV['EMAIL__SENDER_NO_REPLY'] . "\r\n";
+    
         return mail($to, $subject, $message, $headers);
     }
 }
