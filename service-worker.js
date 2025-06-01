a73468753d1abfaec9e359d07afdaf20e44c683b
Omri Edri <edri90@gmail.com>
2025-06-01 17:07:52 +0300

    Comment out fetch event listener in service worker to prevent handling requests to /public

--- service-worker.js
+++ service-worker.js
@@ -7,21 +7,21 @@ self.addEventListener("install", (event) => {
 });
 
 // Fetch event to serve cached assets or fetch from network and cache them
-self.addEventListener("fetch", (event) => {
-    // Only handle requests to /public
-    if (event.request.url.includes("/public/")) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then((cache) => {
-                return cache.match(event.request).then((response) => {
-                    if (response) {
-                        return response;
-                    }
-                    return fetch(event.request).then((networkResponse) => {
-                        cache.put(event.request, networkResponse.clone());
-                        return networkResponse;
-                    });
-                });
-            })
-        );
-    }
-});
+// self.addEventListener("fetch", (event) => {
+//     // Only handle requests to /public
+//     if (event.request.url.includes("/public/")) {
+//         event.respondWith(
+//             caches.open(CACHE_NAME).then((cache) => {
+//                 return cache.match(event.request).then((response) => {
+//                     if (response) {
+//                         return response;
+//                     }
+//                     return fetch(event.request).then((networkResponse) => {
+//                         cache?.put(event.request, networkResponse.clone());
+//                         return networkResponse;
+//                     });
+//                 });
+//             })
+//         );
+//     }
+// });
